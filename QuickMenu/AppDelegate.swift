d92620bf201739053701bf968d7abcb5250d8e40
Jesse Kasky <jesse.kasky@gmail.com>
2015-06-24 18:33:28 -0700

    Initial Commit

---
+++ QuickMenu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QuickMenu
+//
+//  Created by Jesse Kasky on 6/24/15.
+//  Copyright (c) 2015 Codjax. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ QuickMenu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QuickMenu
+//
+//  Created by Jesse Kasky on 6/24/15.
+//  Copyright (c) 2015 Codjax. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
