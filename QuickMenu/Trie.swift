13517d780effd0523855136ec1d4fe8def7de1a7
Jesse Kasky <jesse.kasky@gmail.com>
2018-05-20 21:27:17 -0700

    Add Trie structure for indexing menu commands based on menu path.

--- QuickMenu/Trie.swift
+++ QuickMenu/Trie.swift
@@ -9,26 +9,123 @@
 import Foundation
 
 
-public class TrieNode {
+fileprivate class TrieNode<V> {
 
-  var children:[TrieNode]
-  var isLeaf:Bool
+  let character: Character
+  var children: [TrieNode<V>]
+  var value: V?
 
-  init() {
-    children = []
-    isLeaf = false
+  var isLeaf: Bool {
+    get {
+      return value != nil
+    }
   }
 
+  init(character: Character, value: V? = nil) {
+    self.character = character
+    self.value = value
+    self.children = []
+  }
 }
 
 
-public class Trie {
+/**
+ * Trie structure for matching character sequences to any object.
+ *
+ * A slightly different Trie than a prefix trie, this Trie matches sequences
+ * of characters that are not adjacent and they do not need to be prefixes.
+ */
+public class Trie<V> {
+
+  private let root: TrieNode<V>
 
   init() {
-
+    root = TrieNode<V>(character: "\u{0}")
   }
 
-  func insert() {
+  /**
+   * Inserts a new value into the trie with a given label.
+   */
+  func insert(label: String, value: V) {
+    var node = root
+    for c in label {
+      if node.children.isEmpty {
+        let newChild = TrieNode<V>(character:c)
+        node.children.append(newChild)
+        node = newChild
+        continue
+      }
+      for i in 0..<node.children.count {
+        let child = node.children[i]
+        if child.character == c {
+          node = child
+          break
+        }
+        if c < child.character {
+          let newChild = TrieNode<V>(character:c)
+          node.children.insert(newChild, at:i)
+          node = newChild
+          break
+        }
+        if i == node.children.count - 1 {
+          let newChild = TrieNode<V>(character:c)
+          node.children.append(newChild)
+          node = newChild
+          break
+        }
+      }
+    }
+    node.value = value
+  }
 
+  /**
+   * Finds all values in the trie with labels that match the given sequence.
+   *
+   * The characters in the sequence can be nonadjacent to match a label. All of
+   * the sequence characters must be in the label to match. The results will be
+   * returned in ascending alphabetical order
+   */
+  func find(sequence: String) -> [V] {
+    return find(sequence, root).map { $0.value! }
+  }
+
+  private func find(_ sequence: String, _ node: TrieNode<V>) -> [TrieNode<V>] {
+    var matches: [TrieNode<V>] = []
+
+    // When there is no more sequence return all leafs. This indicates that
+    // a full match has been made up to this point so traverse the remaining
+    // children to find all the leafs.
+    guard sequence.count > 0 else {
+      for child in node.children {
+        if child.isLeaf {
+          matches.append(child)
+        }
+        if child.children.count > 0 {
+          matches += find(sequence, child)
+        }
+      }
+      return matches
+    }
+
+    // When the first character of the sequence matches the current node we have
+    // a partial match.
+    if sequence[sequence.startIndex] == node.character {
+      if node.isLeaf {
+        matches.append(node)
+      }
+      if sequence.count > 1 {
+        let suffix = sequence.suffix(sequence.count - 1)
+        for child in node.children {
+          matches += find(String(suffix), child)
+        }
+      } else {
+        matches += find("", node)
+      }
+    } else {
+      for child in node.children {
+        matches += find(sequence, child)
+      }
+    }
+    return matches
   }
 }
---
+++ QuickMenuTests/TrieTests.swift
@@ -0,0 +1,112 @@
+//
+//  TrieTest.swift
+//  QuickMenuTests
+//
+//  Created by Jesse Kasky on 4/25/18.
+//  Copyright © 2018 Codjax. All rights reserved.
+//
+
+import XCTest
+
+class TrieTest: XCTestCase {
+
+  override func setUp() {
+    super.setUp()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+  }
+
+  func testEmptyTrie() {
+    XCTAssertEqual(Trie<Int>().find(sequence: ""), [])
+  }
+
+  func testNoMatches() {
+    let trie = Trie<Int>()
+    trie.insert(label: "apple", value: 0)
+    trie.insert(label: "orange", value: 1)
+    XCTAssertEqual(trie.find(sequence: "ba"), [])
+  }
+
+  func testFindWithFullValue() {
+    let trie = Trie<Int>()
+    trie.insert(label: "monkey", value: 1)
+    trie.insert(label: "zebra", value: 2)
+    trie.insert(label: "zap", value: 3)
+
+    XCTAssertEqual(trie.find(sequence: "zebra"), [2])
+  }
+
+  func testFindWithEachCharacter() {
+    let trie = Trie<String>()
+    let string = "abc"
+    trie.insert(label: string, value: string)
+    for c in string {
+      XCTAssertEqual(trie.find(sequence: String(c)), [string])
+    }
+  }
+
+  func testFindsAllWithSingleCharacter() {
+    let trie = Trie<Int>()
+    trie.insert(label: "apple", value: 1)
+    trie.insert(label: "banana", value: 2)
+    trie.insert(label: "kiwi", value: 3)
+    trie.insert(label: "orange", value: 4)
+
+    XCTAssertEqual(trie.find(sequence: "p"), [1])
+    XCTAssertEqual(trie.find(sequence: "a"), [1, 2, 4])
+    XCTAssertEqual(trie.find(sequence: "e"), [1, 4])
+  }
+
+  func testFindWithTrailingCharacter() {
+    let trie = Trie<Int>()
+    trie.insert(label: "lion", value: 1)
+    trie.insert(label: "tiger", value: 2)
+    trie.insert(label: "bear", value: 3)
+    trie.insert(label: "baboon", value: 4)
+
+    XCTAssertEqual(trie.find(sequence: "r"), [3, 2])
+  }
+
+  func testDoesNotFindWithDuplicateCharacterSequence() {
+    let trie = Trie<Int>()
+    trie.insert(label: "pole", value: 1)
+
+    XCTAssertEqual(trie.find(sequence: "pp"), [])
+  }
+
+  func testFindWithDuplicateSequence() {
+    let trie = Trie<Int>()
+    trie.insert(label: "apple", value: 1)
+    trie.insert(label: "apps", value: 2)
+    trie.insert(label: "pole", value: 3)
+    trie.insert(label: "pop", value: 4)
+    trie.insert(label: "pps", value: 5)
+
+    XCTAssertEqual(trie.find(sequence:"pp"), [1, 2, 4, 5])
+  }
+
+  func testFindResultsSortedAlphabetically() {
+    let trie = Trie<Int>()
+    trie.insert(label: "the world", value: 2)
+    trie.insert(label: "the world is", value: 3)
+    trie.insert(label: "is the world round", value:1)
+    trie.insert(label: "world", value: 4)
+    trie.insert(label: "is the world", value: 0)
+
+    XCTAssertEqual(trie.find(sequence: "w"), [0, 1, 2, 3, 4])
+  }
+
+  func testFindWithNonAdjacentSequence() {
+    let trie = Trie<Int>()
+    trie.insert(label: "file > new", value: 0)
+    trie.insert(label: "file > open", value: 1)
+    trie.insert(label: "find > next", value: 2)
+    trie.insert(label: "find > previous", value: 3)
+
+    XCTAssertEqual(trie.find(sequence:"fp"), [1, 3])
+    XCTAssertEqual(trie.find(sequence:"fw"), [0])
+    XCTAssertEqual(trie.find(sequence:"fdt"), [2])
+  }
+}
