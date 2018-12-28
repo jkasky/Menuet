a9cf3157038bb28fecfbaf9296692b4cea58acf5
Jesse Kasky <jesse.kasky@gmail.com>
2018-12-28 02:09:14 -0800

    Fix MenuItemCommand Tests

--- QuickMenuTests/MenuItemCommandTests.swift
+++ QuickMenuTests/MenuItemCommandTests.swift
@@ -12,97 +12,97 @@ class MenuItemCommandTest: XCTestCase {
 
   func testNormalCommandModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 0))
+      character: "C", modifiers:Modifiers(rawValue: 0))
     XCTAssertEqual(c.stringValue, "⌘C")
   }
 
   func testShiftAndCommandModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 1))
+      character: "C", modifiers:Modifiers(rawValue: 1))
     XCTAssertEqual(c.stringValue, "⇧⌘C")
   }
 
   func testOptionAndCommandModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 2))
+      character: "C", modifiers:Modifiers(rawValue: 2))
     XCTAssertEqual(c.stringValue, "⌥⌘C")
   }
 
   func testOptionShiftAndCommandModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 3))
+      character: "C", modifiers:Modifiers(rawValue: 3))
     XCTAssertEqual(c.stringValue, "⌥⇧⌘C")
   }
 
   func testControlAndCommandModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 4))
+      character: "C", modifiers:Modifiers(rawValue: 4))
     XCTAssertEqual(c.stringValue, "⌃⌘C")
   }
 
   func testControlShiftAndCommandModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 5))
+      character: "C", modifiers:Modifiers(rawValue: 5))
     XCTAssertEqual(c.stringValue, "⌃⇧⌘C")
   }
 
   func testControlOptionAndCommandModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 6))
+      character: "C", modifiers:Modifiers(rawValue: 6))
     XCTAssertEqual(c.stringValue, "⌃⌥⌘C")
   }
 
   func testControlOptionShiftAndCommandModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 7))
+      character: "C", modifiers:Modifiers(rawValue: 7))
     XCTAssertEqual(c.stringValue, "⌃⌥⇧⌘C")
   }
 
   func testNoCommandModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 8))
+      character: "C", modifiers:Modifiers(rawValue: 8))
     XCTAssertEqual(c.stringValue, "C")
   }
 
   func testShiftModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 9))
+      character: "C", modifiers:Modifiers(rawValue: 9))
     XCTAssertEqual(c.stringValue, "⇧C")
   }
 
   func testOptionModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 10))
+      character: "C", modifiers:Modifiers(rawValue: 10))
     XCTAssertEqual(c.stringValue, "⌥C")
   }
 
   func testOptionAndShiftModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 11))
+      character: "C", modifiers:Modifiers(rawValue: 11))
     XCTAssertEqual(c.stringValue, "⌥⇧C")
   }
 
   func testControlModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 12))
+      character: "C", modifiers:Modifiers(rawValue: 12))
     XCTAssertEqual(c.stringValue, "⌃C")
   }
 
   func testControlAndShiftModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 13))
+      character: "C", modifiers:Modifiers(rawValue: 13))
     XCTAssertEqual(c.stringValue, "⌃⇧C")
   }
 
   func testControlAndOptionModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 14))
+      character: "C", modifiers:Modifiers(rawValue: 14))
     XCTAssertEqual(c.stringValue, "⌃⌥C")
   }
 
   func testControlOptionAndShiftModifier() {
     let c = MenuItemCommand(
-      character: "C", modifiers:AXMenuItemModifiers(rawValue: 15))
+      character: "C", modifiers:Modifiers(rawValue: 15))
     XCTAssertEqual(c.stringValue, "⌃⌥⇧C")
   }
 }
